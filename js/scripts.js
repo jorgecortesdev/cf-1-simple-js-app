dd573259dc9b198e775bbafff7ba1a4424db08f3
Jorge Cortés <jorge.cortes@gmail.com>
2024-03-14 17:04:34 -0700

    Enhanced design and visual appeal of the modal
    - Implemented aesthetic enhancements to the modal, including layout adjustments, color scheme updates, and typography improvements

--- js/scripts.js
+++ js/scripts.js
@@ -50,7 +50,6 @@ const pokemonRepository = (function () {
       return response.json();
     }).then(function (details) {
       pokemon.id = `#${id.padStart(4, '0')}`;
-      pokemon.imageUrl = details.sprites.front_default;
       pokemon.types = details.types.map(({ type }) => type.name);
       pokemon.cover = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
       add(pokemon)
@@ -65,8 +64,16 @@ const pokemonRepository = (function () {
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
       item.height = details.height;
+      item.weight = details.weight;
+      item.species = details.species.name;
+      item.abilities = details.abilities.map(({ ability }) => ability.name);
+      item.stats = details.stats.map(item => {
+        return {
+          name: item.stat.name,
+          value: item.base_stat
+        }
+      });
       _hideLoadingMessage();
     }).catch(function (e) {
       console.error(e);
@@ -147,23 +154,123 @@ const pokemonRepository = (function () {
   }
 
   function _showModal(pokemon) {
-    let titleElement = document.querySelector('#exampleModalLabel');
+    let modalContent = document.querySelector('.modal-content');
+    modalContent.className = '';
+    modalContent.classList.add('modal-content', `type-bg-${pokemon.types[0]}`);
+
+    let titleElement = document.createElement('h2');
     titleElement.innerText = pokemon.name;
 
+    let listTypesElement = document.createElement('ul');
+    listTypesElement.classList.add('panel__types');
+
+    pokemon.types.forEach(function (type) {
+      let typeElement = document.createElement('li');
+      typeElement.classList.add('panel__badge');
+      typeElement.innerText = type;
+      listTypesElement.appendChild(typeElement);
+    });
+
+    let headerTitleElement = document.createElement('div');
+    headerTitleElement.classList.add('modal-header-title');
+    headerTitleElement.appendChild(titleElement);
+    headerTitleElement.appendChild(listTypesElement);
+
+    let idElement = document.createElement('div');
+    idElement.classList.add('modal-header-id');
+    idElement.innerText = pokemon.id;
+
+    let headerContainerElement = document.createElement('div');
+    headerContainerElement.appendChild(headerTitleElement);
+    headerContainerElement.appendChild(idElement);
+
     let imageElement = document.createElement('img');
-    imageElement.src = pokemon.imageUrl;
+    imageElement.src = pokemon.cover;
 
-    let heightElement = document.createElement('p');
-    heightElement.innerText = `Height: ${pokemon.height}`;
+    let modalHeaderElement = document.querySelector('.modal-header');
+    modalHeaderElement.innerHTML = '';
+    modalHeaderElement.appendChild(headerContainerElement);
+    modalHeaderElement.appendChild(imageElement);
 
-    let typesElement = document.createElement('p');
-    typesElement.innerText = `Types: ${pokemon.types.join(', ')}`;
+    let definitionListElement = document.createElement('dl');
 
-    let bodyElement = document.querySelector('.modal-body');
-    bodyElement.innerHTML = '';
-    bodyElement.appendChild(imageElement);
-    bodyElement.appendChild(heightElement);
-    bodyElement.appendChild(typesElement);
+    let speciesTitleElement = document.createElement('dt');
+    speciesTitleElement.innerText = 'Species';
+    definitionListElement.appendChild(speciesTitleElement);
+    let speciesValueElement = document.createElement('dd');
+    speciesValueElement.innerText = pokemon.species;
+    definitionListElement.appendChild(speciesValueElement);
+
+    let heightTitleElement = document.createElement('dt');
+    heightTitleElement.innerText = 'Height';
+    definitionListElement.appendChild(heightTitleElement);
+    let heightValueElement = document.createElement('dd');
+    heightValueElement.innerText = pokemon.height;
+    definitionListElement.appendChild(heightValueElement);
+
+    let weightTitleElement = document.createElement('dt');
+    weightTitleElement.innerText = 'Weight';
+    definitionListElement.appendChild(weightTitleElement);
+    let weightValueElement = document.createElement('dd');
+    weightValueElement.innerText = pokemon.weight;
+    definitionListElement.appendChild(weightValueElement);
+
+    let abilitiesTitleElement = document.createElement('dt');
+    abilitiesTitleElement.innerText = 'Abilities';
+    definitionListElement.appendChild(abilitiesTitleElement);
+    let abilitiesValueElement = document.createElement('dd');
+    abilitiesValueElement.innerText = pokemon.abilities.join(", ");
+    definitionListElement.appendChild(abilitiesValueElement);
+
+    let tabContentElement = document.querySelector('#myTabContent');
+    tabContentElement.innerHTML = '';
+
+    let aboutTabPaneElement = document.createElement('div');
+    aboutTabPaneElement.classList.add('tab-pane', 'fade', 'show', 'active');
+    aboutTabPaneElement.setAttribute('id', 'about-tab-pane')
+    aboutTabPaneElement.appendChild(definitionListElement);
+    tabContentElement.appendChild(aboutTabPaneElement);
+
+    let statsDefinitionListElement = document.createElement('dl');
+    pokemon.stats.forEach(stat => {
+      let statTitleElement = document.createElement('dt');
+      statTitleElement.innerText =
+        stat.name === 'special-attack'
+        ? 'sp. atk'
+        : stat.name === 'special-defense'
+        ? 'sp. def'
+        : stat.name;
+      statsDefinitionListElement.appendChild(statTitleElement);
+      let statValueElement = document.createElement('dd');
+      statValueElement.classList.add('d-flex', 'flex-row', 'gap-3', 'align-items-center');
+      let statValueContainer = document.createElement('span');
+      statValueContainer.innerText = stat.value;
+      statValueElement.appendChild(statValueContainer);
+
+      let progressContainerElement = document.createElement('div');
+      progressContainerElement.classList.add('d-block', 'w-100');
+      let progressElement = document.createElement('span');
+      progressElement.classList.add('progress');
+      progressElement.style = 'height: 16px;';
+      let progressBarElement = document.createElement('span');
+      progressBarElement.classList.add('progress-bar', `type-bg-${pokemon.types[0]}`);
+      progressBarElement.style = `width: ${stat.value}%`;
+      progressElement.appendChild(progressBarElement);
+      progressContainerElement.appendChild(progressElement);
+      statValueElement.appendChild(progressContainerElement);
+
+      statsDefinitionListElement.appendChild(statValueElement);
+    });
+
+    let statsTabPaneElement = document.createElement('div');
+    statsTabPaneElement.classList.add('tab-pane', 'fade');
+    statsTabPaneElement.setAttribute('id', 'stats-tab-pane')
+    statsTabPaneElement.appendChild(statsDefinitionListElement);
+    tabContentElement.appendChild(statsTabPaneElement);
+
+    // reset tab
+    const trigger = document.querySelector('#myTab button[data-bs-target="#about-tab-pane"]');
+    bootstrap.Tab.getInstance(trigger).show();
   }
 
   document.querySelector('#search').addEventListener('input', (e) => {
